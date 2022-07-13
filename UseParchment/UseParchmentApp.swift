830a80fea08ab30dda4e39354db4de5dc7ef7589
JunichiroNoguchi <nanao@prosoft.co.jp>
2022-07-13 18:08:49 +0900

    Initial Commit

---
+++ UseParchment/UseParchmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseParchmentApp.swift
+//  UseParchment
+//
+//  Created by prosoft mac 03 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct UseParchmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
